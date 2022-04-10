cd1d7734570c2de8de3445d51a4ad440987e3697
sunghwanKim <shkcreate@naver.com>
2022-04-10 20:17:22 +0900

    커밋

--- src/main/java/com/nhnacademy/project/parking/CompositeDiscounter.java
+++ src/main/java/com/nhnacademy/project/parking/CompositeDiscounter.java
@@ -1,2 +1,25 @@
-package com.nhnacademy.project.parking;public class CompositeDiscounter {
+package com.nhnacademy.project.parking;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CompositeDiscounter implements Discountable {
+    private final List<Discountable> discountables;
+
+    public CompositeDiscounter(List<Discountable> discountableList) {
+        this.discountables = discountableList;
+    }
+
+    public CompositeDiscounter(DiscountPolicyRepository repository, String... codes) {
+        this.discountables = repository.findByCodes(Arrays.asList(codes));
+    }
+
+    @Override
+    public long getDiscountAmt(long originAmt) {
+        long discountAmt = 0L;
+        for (Discountable discountable : discountables) {
+            discountAmt += discountable.getDiscountAmt(originAmt);
+        }
+        return discountAmt;
+    }
 }
