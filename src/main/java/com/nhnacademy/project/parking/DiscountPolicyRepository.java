99841a440b61506c7e742c780e6bd1ec6a070257
sunghwanKim <shkcreate@naver.com>
2022-04-10 20:17:46 +0900

    커밋

--- src/main/java/com/nhnacademy/project/parking/DiscountPolicyRepository.java
+++ src/main/java/com/nhnacademy/project/parking/DiscountPolicyRepository.java
@@ -1,2 +1,24 @@
-package com.nhnacademy.project.parking;public interface DiscountPolicyRepository {
+package com.nhnacademy.project.parking;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public interface DiscountPolicyRepository {
+
+    void insert(DiscountPolicy source);
+
+    DiscountPolicy findByCode(String code);
+
+    default List<Discountable> findByCodes(List<String> codes) {
+        return codes.stream()
+            .map(this::findByCode)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+    }
+
+
+    void deleteByCode(String code);
 }
+
+
