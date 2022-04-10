281d0fefabb6773cca08abf511e111f596df9695
sunghwanKim <shkcreate@naver.com>
2022-04-10 20:17:33 +0900

    커밋

--- src/main/java/com/nhnacademy/project/parking/Discount.java
+++ src/main/java/com/nhnacademy/project/parking/Discount.java
@@ -1,2 +1,22 @@
-package com.nhnacademy.project.parking;public class Discount {
+package com.nhnacademy.project.parking;
+
+public class Discount {
+    private final long amount;
+
+    private Discount(long discountAmt) {
+        this.amount = discountAmt;
+    }
+
+    public static Discount of(long discountAmt) {
+        return new Discount(discountAmt);
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return amount + "원";
+    }
 }
